c02ab67ae5add910d516c7eb39cb2e8cd686a2d9
Lucas Freitas <lucas@lucasfreitas.com.br>
2022-04-18 12:32:59 +0200

    First release of the app.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 const path = require('path');
-const {app, BrowserWindow, Menu} = require('electron');
+const {app, BrowserWindow, Menu, protocol, shell} = require('electron');
 /// const {autoUpdater} = require('electron-updater');
 const {is} = require('electron-util');
 const unhandled = require('electron-unhandled');
@@ -8,13 +8,25 @@ const debug = require('electron-debug');
 const contextMenu = require('electron-context-menu');
 const config = require('./config.js');
 const menu = require('./menu.js');
+const windows = require('./windows.js');
 
 unhandled();
-debug();
 contextMenu();
+if (is.windows) {
+  windows().then();
+}
 
 // Note: Must match `build.appId` in package.json
-app.setAppUserModelId('com.company.AppName');
+app.setAppUserModelId('me.lucasfreitas.WhatsAppCaller');
+if (process.defaultApp) {
+  if (process.argv.length >= 2) {
+    app.setAsDefaultProtocolClient('tel', process.execPath, [path.resolve(process.argv[1])])
+    app.setAsDefaultProtocolClient('callto', process.execPath, [path.resolve(process.argv[1])])
+  }
+} else {
+  app.setAsDefaultProtocolClient('tel');
+  app.setAsDefaultProtocolClient('callto');
+}
 
 // Uncomment this before publishing your first version.
 // It's commented out as it throws an error if there are no published versions.
@@ -34,12 +46,12 @@ const createMainWindow = async () => {
 	const win = new BrowserWindow({
 		title: app.name,
 		show: false,
-		width: 600,
-		height: 400
+		width: 1,
+		height: 1
 	});
 
 	win.on('ready-to-show', () => {
-		win.show();
+		// win.show();
 	});
 
 	win.on('closed', () => {
@@ -47,8 +59,6 @@ const createMainWindow = async () => {
 		// For multiple windows store them in an array
 		mainWindow = undefined;
 	});
-
-	await win.loadFile(path.join(__dirname, 'index.html'));
 
 	return win;
 };
@@ -69,9 +79,7 @@ app.on('second-instance', () => {
 });
 
 app.on('window-all-closed', () => {
-	if (!is.macos) {
-		app.quit();
-	}
+  app.quit();
 });
 
 app.on('activate', async () => {
@@ -80,11 +88,33 @@ app.on('activate', async () => {
 	}
 });
 
+const parseNumber = (number) => {
+  const cleanRegex = new RegExp(/^(\+)|\D/, 'g');
+  let cleanNumber = String(number).trim();
+  cleanNumber.replace(/^(tel|callto):\/\//, '');
+  cleanNumber = cleanNumber.replace(cleanRegex, '$1');
+  if (cleanNumber.startsWith("0")) {
+    cleanNumber = "49" + cleanNumber.substr(1);
+  }
+  return cleanNumber;
+};
+
+app.on('open-url', async (event, url) => {
+  await handleWhatsAppLink(mainWindow, parseNumber(url));
+  app.quit();
+});
+
+const handleWhatsAppLink = async (win, string) => {
+  await shell.openExternal(`whatsapp://send?phone=${string}`);
+}
+
+const noop =
+  (request, callback) => {};
+
 (async () => {
 	await app.whenReady();
+  protocol.registerStringProtocol('callto', noop);
+  protocol.registerStringProtocol('tel', noop);
 	Menu.setApplicationMenu(menu);
 	mainWindow = await createMainWindow();
-
-	const favoriteAnimal = config.get('favoriteAnimal');
-	mainWindow.webContents.executeJavaScript(`document.querySelector('header p').textContent = 'Your favorite animal is ${favoriteAnimal}'`);
 })();
